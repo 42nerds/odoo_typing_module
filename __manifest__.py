3d43bf7f1e6b4c4c6fcfd558573cadce40e3c110
Melvin-John <melvinjo@42nerds.com>
2020-08-26 12:35:47 +0000

    [IMPROV] prepare for odoo module

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "42 N.E.R.D.S. Typing Module",
+
+    'summary': """
+        This module adds a snipped for the websitebuilder""",
+
+    'description': """
+        This module types each letter until the word is completely.
+        The complete word remains for a short moment, 
+        until it will be removed piece by piece and a new word is typed in again.
+    """,
+
+    'author': "42 N.E.R.D.S.",
+    'website': "https://www.42nerds.com",
+    'category': 'Apps',
+    'version': '0.0.1',
+    'depends': [
+        'base',
+        'website',
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/templates.xml',
+    ],
+    # 'demo': [
+    #     'demo/demo.xml'
+    # ],
+    'installable': True,
+    'auto_install': False,
+}
